eb2d0ea25ff602dbf1de8c531e13a9c74ad44de0
Carlos <carlosd.solano98@gmail.com>
2026-09-03 23:35:25 -0500

    fix: configurar root de Vite para index.html

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  root: 'Repo90-main',
   build: {
-    target: "es2020"
-  }
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
 });
